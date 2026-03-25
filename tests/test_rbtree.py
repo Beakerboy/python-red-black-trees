52719bb133e26490daefd0296e413231181c6b92
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-25 14:36:37 -0400

    Update test_rbtree.py

--- tests/test_rbtree.py
+++ tests/test_rbtree.py
@@ -155,7 +155,7 @@ def test_rotation4() -> None:
     assert bst.get_root().key == 2
 
 
-test delete_rotation1() -> None
+def test_delete_rotation1() -> None
     bst = RedBlackTree()
     bst.insert(1)
     bst.insert(2)
