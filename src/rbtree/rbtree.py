17ebe1ee6e56c2188eafee5ee501b2f7c59eda76
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-27 11:22:43 -0400

    Update rbtree.py

--- src/rbtree/rbtree.py
+++ src/rbtree/rbtree.py
@@ -449,8 +449,8 @@ class RedBlackTree():
         """
         basenode = []
         if not node.is_null():
-            basenode = self._in_order_helper(node.left, include_nulls)
-            basenode.extend(self._in_order_helper(node.right, include_nulls))
+            basenode = self._post_order_helper(node.left, include_nulls)
+            basenode.extend(self._post_order_helper(node.right, include_nulls))
             basenode.extend([node])
         elif include_nulls:
             basenode = [node]
