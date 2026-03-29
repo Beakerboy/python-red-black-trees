0305bfaa28dedbb071857f7691e6a2457fbe2a21
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-29 16:46:00 -0400

    Update test_node.py

--- tests/test_node.py
+++ tests/test_node.py
@@ -5,3 +5,12 @@ def test_lt() -> None:
     zero = Node(0)
     one = Node(1)
     assert zero < one
+
+def test_str() -> None:
+    zero = Node(0)
+    assert str(zero) == "0"
+
+
+def test_repr() -> None:
+    zero = Node(0)
+    assert repr(zero) == "Key: 0"
